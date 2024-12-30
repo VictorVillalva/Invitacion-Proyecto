0493273ab831f98108acaab9af52803c6c7d4b1b
Victor Villalva <victor.villalva@sdnet.com.mx>
2024-12-29 22:37:57 -0600

    Agregacion de contacto

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Invitacion from "@/components/pre-invitacion/invitacion";
 
 export default function Home() {
   return (
-    <div className="flex flex-col px-6 py-[80px]">
-      <Invitacion />
-    </div>
+    <>
+      <div className="flex flex-col px-6 py-[80px]">
+        <Invitacion />
+      </div>
+      <div className="w-full flex flex-col justify-center items-center p-4">
+        <p className="text-neutrals-100 font-semibold text-xs text-center">Derechos Reservados 2024 </p>
+        <p className="text-neutrals-100 font-semibold text-xs text-center">Contacto  📞961 374 4444 ✉️venturinaaylin@gmail.com</p>
+      </div>
+    </>
   );
 }
